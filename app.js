4832b24e79c29920be3b655f3133595de78cb574
OrdepG <155494650+OrdepG@users.noreply.github.com>
2025-04-30 10:02:10 +0100

    update

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ let input = document.querySelector("input");
 let todos = document.querySelector(".todos");
 
 function getTodo(todoObj) {
-  // Creating New Elements
   let todo = document.createElement("div");
   let textEl = document.createElement("span");
 
-  // Setting values & Styles
   textEl.innerHTML = todoObj.text;
 
   if (todoObj.done) {
@@ -18,15 +16,14 @@ function getTodo(todoObj) {
     textEl.classList.toggle("done");
     toggleTodoStatus(todoObj.text);
   });
-  // Appending Our Element To The DOM
+
   todo.appendChild(textEl);
 
   let closeEl = document.createElement("span");
   closeEl.innerHTML = "&times;";
   closeEl.classList.add("delete");
 
-  // Attaching Events
-  closeEl.addEventListener("click", function (e) {
+  closeEl.addEventListener("click", function () {
     todos.removeChild(todo);
     removeTodoFromLocal(todoObj.text);
   });
@@ -37,20 +34,18 @@ function getTodo(todoObj) {
 }
 
 form.addEventListener("submit", (e) => {
-  // preventing the default behavior
   e.preventDefault();
   let value = input.value;
   if (!value.trim()) return;
 
-  const todoObj = { text: value, done: false};
+  const todoObj = { text: value, done: false };
   todos.appendChild(getTodo(todoObj));
-
-  saveTodoToLocal(todoObj)
+  saveTodoToLocal(todoObj);
   input.value = "";
 });
- 
+
 function saveTodoToLocal(todoObj) {
-  let savedTodos = JSON.parse(localStorage.getItem("todos")) ||  [];
+  let savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
   savedTodos.push(todoObj);
   localStorage.setItem("todos", JSON.stringify(savedTodos));
 }
@@ -64,18 +59,18 @@ function loadTodosFromLocal() {
   });
 }
 
-loadTodosFromLocal();
-
 function removeTodoFromLocal(text){
   let savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
-  savedTodos = savedTodos.filter(todo => todo !== text);
+  savedTodos = savedTodos.filter(todo => todo.text !== text);
   localStorage.setItem("todos", JSON.stringify(savedTodos));
 }
 
 function toggleTodoStatus(text) {
   let savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
   savedTodos = savedTodos.map(todo =>
-    todo.text === text ? { ...todo, done: !todo.done} : todo
+    todo.text === text ? { ...todo, done: !todo.done } : todo
   );
   localStorage.setItem("todos", JSON.stringify(savedTodos));
 }
+
+loadTodosFromLocal();
